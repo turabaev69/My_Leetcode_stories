335fa13ee082355836985c092d8667d7e77a9f8a
Turabaev Azizbek <kbibi0865@gmail.com>
2024-10-01 04:29:46 +0900

    Update Palindrome Number.java

--- Java/medium/Palindrome Number.java
+++ Java/medium/Palindrome Number.java
@@ -1,1 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        number = str(x)
+        divide = len(number) // 2
+        for i in range(divide):
+            if number[i] != number[-i -1]:
+                return False
+        return True
 
+        
+        

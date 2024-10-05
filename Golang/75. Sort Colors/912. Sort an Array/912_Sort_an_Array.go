04ce85f8dd4c4c4ac81895308df18a0284a497d8
Turabaev Azizbek <kbibi0865@gmail.com>
2024-10-05 23:47:03 +0900

    Create 912_Sort_an_Array.go

---
+++ Golang/75. Sort Colors/912. Sort an Array/912_Sort_an_Array.go
@@ -0,0 +1,15 @@
+func sortArray(nums []int) []int {
+
+    for j := 0; j < len(nums); j++ {
+
+        for i:= 0; i< len(nums)-1; i++{
+        if nums[i] > nums[i+1]{
+          
+           nums[i], nums[i+1] = nums[i+1], nums[i]
+        }
+    }
+
+    }
+    return nums;
+    
+}

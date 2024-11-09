4a2634844a781b9ee2b55ff7a2a17d5a1c622866
Turabaev Azizbek <kbibi0865@gmail.com>
2024-11-09 23:54:05 +0900

    Create 365_Water_and_Jug_Problem.java

---
+++ Java/medium/365_Water_and_Jug_Problem/365_Water_and_Jug_Problem.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+        if (target > x + y) return false;
+        if (target == 0) return true;
+        return target % gcd(x, y) == 0;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
